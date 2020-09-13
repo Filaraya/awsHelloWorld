8b13878fd663f65587a7cf4daf20d5586204737c
Filmon Araya <40590856+Filaraya@users.noreply.github.com>
2020-09-13 11:55:44 -0700

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello World this is my first aws message\n')
+   response.end('Hello World! I am Filmon, this is my first aws message\n')
 }).listen(3333)
 
 // Console will print the message
